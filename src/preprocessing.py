1795aa28383c82d2c1e689b342697dee2dbd7ec9
Chance Krueger <chancekrueger@arizona.edu>
2026-04-14 16:51:11 -0700

    Implemented load_audio_file

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,8 +1,12 @@
+from scipy.io import wavfile
+
+
 # Noise reduction, filtering, normalization
 
 # Read one .wav file and return the actual waveform plus its sample rate.
-def load_audio_file():
-    pass
+def load_audio_file(file_path):
+    sample_rate, audio = wavfile.read(file_path)
+    return audio, sample_rate
 
 
 # Look at one loaded clip and understand what dealing with.
@@ -43,3 +47,10 @@ def preprocess_audio():
 # Apply preprocessing to every file in dataset index.
 def preprocess_dataset():
     pass
+
+
+def main():
+
+    print(load_audio_file("/Users/chancekrueger/Documents/GitHub/EchoRide/data/raw/FrontPass/FrontPass_L2R_HeavyWind.wav"))
+
+main()
