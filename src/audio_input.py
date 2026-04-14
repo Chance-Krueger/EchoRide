95a53f990be829a3e6d9182bead9a9a90aa00e72
Chance Krueger <chancekrueger@arizona.edu>
2026-04-14 14:40:20 -0700

    Added function signatures to audio_input.py file

--- src/audio_input.py
+++ src/audio_input.py
@@ -1,1 +1,26 @@
 # Handles loading audio files or streaming audio
+
+# This function decides where raw audio lives.
+def get_raw_data_path():
+    pass
+
+# This function finds the class names from folder structure.
+def get_category_folders():
+    pass
+
+# Given one category folder, find all .wav files in it.
+def get_wav_files_in_category():
+    pass
+
+# This turns a category folder into a label.
+def extract_label_from_folder():
+    pass
+
+# Main function in file that combines all the smaller functions and creates the dataset list.
+def build_file_index():
+    pass
+
+# Before loading actual audio, it is really useful to print a basic summary.
+def summarize_dataset():
+    pass
+
