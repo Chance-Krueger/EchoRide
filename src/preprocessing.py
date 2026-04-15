411a89de59ca31435cf62de68dae0c2bd55bbb09
Chance Krueger <chancekrueger@arizona.edu>
2026-04-14 17:24:55 -0700

    resample_audio and convert_to_mono

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,6 @@
 from scipy.io import wavfile
 import soundfile as sf
+import librosa
 
 
 
@@ -24,15 +25,25 @@ def inspect_audio_properties(audio, sample_rate, file_path):
         "channels": info.channels
     }
 
-
 # Make every clip use the same sample rate.
-def resample_audio():
-    pass
+def resample_audio(audio, original_sr, target_sr, file_path):
+    if original_sr == target_sr:
+        return audio, original_sr
+    
+    resampled_audio = librosa.resample(
+        y=audio,
+        orig_sr=original_sr,
+        target_sr=target_sr
+    )
+
+    return resampled_audio, target_sr
 
 
 # Make sure every clip has one channel.
-def convert_to_mono():
-    pass
+def convert_to_mono(audio):
+    if audio.ndim == 1:
+        return audio
+    return audio.mean(axis=1)
 
 # Remove unnecessary quiet sections at the beginning and end.
 def trim_silence():
@@ -61,9 +72,27 @@ def preprocess_dataset():
 
 
 def main():
+    file_path = "/Users/chancekrueger/Documents/GitHub/EchoRide/data/raw/FrontPass/FrontPass_L2R_HeavyWind.wav"
 
-    audio, sample_rate = load_audio_file("/Users/chancekrueger/Documents/GitHub/EchoRide/data/raw/FrontPass/FrontPass_L2R_HeavyWind.wav")
+    # Load audio
+    audio, sample_rate = load_audio_file(file_path)
 
-    print(inspect_audio_properties(audio, sample_rate, "/Users/chancekrueger/Documents/GitHub/EchoRide/data/raw/FrontPass/FrontPass_L2R_HeavyWind.wav"))
+    print("=== BEFORE RESAMPLING ===")
+    print(inspect_audio_properties(audio, sample_rate, file_path))
+
+    # Choose a target sample rate
+    target_sr = 16000
+
+    # Resample
+    audio = convert_to_mono(audio)
+    resampled_audio, new_sr = resample_audio(audio, sample_rate, target_sr, file_path)
+
+
+    print("\n=== AFTER RESAMPLING ===")
+    print({
+        "sample_rate": new_sr,
+        "num_samples": len(resampled_audio),
+        "duration": len(resampled_audio) / new_sr
+    })
 
 main()
