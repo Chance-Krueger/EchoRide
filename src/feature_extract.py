5a02eb9526c4f5409e0504c88fca0e226b0e1f79
Chance Krueger <chancekrueger@arizona.edu>
2026-04-15 12:59:37 -0700

    Made core function signatures

--- src/feature_extract.py
+++ src/feature_extract.py
@@ -1,1 +1,34 @@
-# Extracts features needed for direction detection
+### Extracts features needed for direction detection
+
+
+# Compute MFCCs for one audio clip and summarize them.
+def extract_mfcc_features():
+    pass
+
+# Compute RMS energy and summarize it
+def extract_rms_feature():
+    pass
+
+# Summarize where the energy sits in the frequency spectrum.
+def extract_spectral_centroid_feature():
+    pass
+
+# Measure noisiness / signal roughness
+def extract_zero_crossing_feature():
+    pass
+
+# Summarize the spread of frequencies
+def extract_spectral_bandwidth_feature():
+    pass
+
+# Summarize the upper-end frequency boundary of most energy
+def extract_spectral_rolloff_feature():
+    pass
+
+# main one-file feature extractor
+def extract_features_from_audio():
+    pass
+
+# Take the processed dataset and convert it into model-ready data
+def extract_features_from_dataset():
+    pass
